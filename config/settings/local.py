3dc1351f097efdc9b50b5f0c9a205563932fa924
seud <seudsahm1@gmail.com>
2026-06-27 17:22:04 +0300

    chore(settings): configure local settings for new phases

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
     default="Y0AByo2HIGKgmImZg8v5XtLfTBZPMa8lAjYyx1Iy9vKDXw7IhzJZEsHRMQZYUl6d",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
